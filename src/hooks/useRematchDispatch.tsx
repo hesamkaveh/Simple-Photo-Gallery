888a7fcdfd4118cdd06f66ab10f5b0647d58709b
hesam kaveh <hk.hesam.hk@gmail.com>
2022-05-29 09:09:07 +0430

    fixed pipeline error

--- src/hooks/useRematchDispatch.tsx
+++ src/hooks/useRematchDispatch.tsx
@@ -1,7 +1,8 @@
 import {Dispatch} from "../store";
 import {useDispatch} from "react-redux";
 
-const useRematchDispatch = <D extends {}, MD>(selector: (dispatch: Dispatch) => MD) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const useRematchDispatch = <_ extends {}, MD>(selector: (dispatch: Dispatch) => MD) => {
     const dispatch = useDispatch<Dispatch>()
     return selector(dispatch)
 }
